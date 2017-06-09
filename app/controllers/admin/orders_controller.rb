71ce488718ef91dcc55789e8b318b7e4f0469205
hy <hsx0916@me.com>
2017-06-09 10:36:21 +0800

    add admin/orders

---
+++ app/controllers/admin/orders_controller.rb
@@ -0,0 +1,10 @@
+class Admin::OrdersController <
+  layout "admin"
+
+  before_action :authenticate_user!
+  before_action :admin_required
+
+  def index
+    @orders = Order.order("id DESC")
+  end
+end
