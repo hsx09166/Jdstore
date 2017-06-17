bd3ad1a9b699bcc37fe0d47881c45522485c0450
hy <hsx0916@me.com>
2017-06-17 16:50:05 +0800

    config qiniu

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,10 @@
+CarrierWave.configure do |config|
+  config.storage             = :qiniu
+  config.qiniu_access_key    = ENV[yLE1pJBPpISCjyTrIrP4eeneEttdHM-p6XATZhIc]
+  config.qiniu_secret_key    = ENV[tEj9jp55o5hHwEw491mJMIqJkEhik7wFF9gLscbR]
+  config.qiniu_bucket        = ENV[jdstore-one]
+  config.qiniu_bucket_domain = ENV[orond511s.bkt.clouddn.com]
+  config.qiniu_block_size    = 4*1024*1024
+  config.qiniu_protocol      = "http"
+  config.qiniu_up_host       = "http://up.qiniug.com"  #选择不同的区域时，up.qiniug.com 不同
+end
