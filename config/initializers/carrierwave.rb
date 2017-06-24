d1977f735463782316a8e097e7bb97178e0f6fee
hy <hsx0916@me.com>
2017-06-24 14:46:17 +0800

    figaro

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|
   config.storage             = :qiniu
-  config.qiniu_access_key    = ENV["yLE1pJBPpISCjyTrIrP4eeneEttdHM-p6XATZhIc"]
-  config.qiniu_secret_key    = ENV["tEj9jp55o5hHwEw491mJMIqJkEhik7wFF9gLscbR"]
-  config.qiniu_bucket        = ENV["jdstore-one"]
-  config.qiniu_bucket_domain = ENV["orond511s.bkt.clouddn.com"]
+  config.qiniu_access_key    = ENV["qiniu_access_key"]
+  config.qiniu_secret_key    = ENV["qiniu_secret_key"]
+  config.qiniu_bucket        = ENV["qiniu_bucket"]
+  config.qiniu_bucket_domain = ENV["qiniu_bucket_domain"]
   config.qiniu_block_size    = 4*1024*1024
   config.qiniu_protocol      = "http"
   config.qiniu_up_host       = "http://up-z2.qiniu.com"  #我是华南
